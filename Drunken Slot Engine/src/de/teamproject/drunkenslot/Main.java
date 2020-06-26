bae89822d80ee5c18b175814ed659abd7d860474
dhaacke <dhaacke@uni-paderborn.de>
2020-06-26 12:29:30 +0200

    Updated PrintSlot, now working on "animations" for cmd

--- Drunken Slot Engine/src/de/teamproject/drunkenslot/Main.java
+++ Drunken Slot Engine/src/de/teamproject/drunkenslot/Main.java
@@ -1,9 +1,11 @@
 package de.teamproject.drunkenslot;
 
+import de.teamproject.drunkenslot.cmdclient.DSCmdClient;
+
 public class Main 
 {
 	public static void main(String[] args) 
 	{
-		
+		DSCmdClient cmdClient = new DSCmdClient();
 	}
 }
